586a781fb31e1f8624d5a8348e3265cb435cfa11
Andrew Keiser <webdev410@gmail.com>
2021-06-12 11:13:22 -0700

    added questions

---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+// commonly used data types DO NOT include:
+
+// strings
+//booleans
+//alerts
+//numbers
+
+
+// The condition in an if/else statement is enclosed within ____.
+
+// quotes
+// curly brackets
+// parentheses
+// square brackets
+
+
+// Arrays in JavaScript can be used to store ____.
+
+// numbers and strings
+// other arrays
+// booleans
+// all of the above
+
+
+// String values must be enclosed within _____ when being assigned to variables.
+
+// commas
+// curly brackets
+// quotes
+// parentheses
